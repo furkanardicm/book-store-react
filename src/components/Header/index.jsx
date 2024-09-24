156756a77db2954d6ed85f60c6f1325d1a28435d
Muhammed Furkan Ardıç <furkanardcm@gmail.com>
2024-09-24 12:41:25 +0300

    Responsive Home Sayfası için stil düzenlemeleri

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,22 +14,22 @@ function index() {
   const styleTheme =  {backgroundColor: theme.primary}
   return (
     <>
-      <nav className="text-nowrap w-screen h-16 bg-[#FAFAFA] relative px-6 flex justify-between font-medium text-base items-center font-lalezar antialiased">
+      <nav className="w-screen h-16 bg-[#FAFAFA] max-md:justify-between relative px-6 flex justify-between font-medium text-base items-center font-lalezar antialiased">
           <div className="h-full flex items-center"><a href="/"><img className='h-[50px]' src="/black-logo.png" alt="LOGO - kitapcim.io" srcset="" /></a></div>
       
-          <div className="w-[354px] h-11 flex items-center rounded-sm">
+          <div className="w-[354px] h-11 flex items-center rounded-sm max-md:hidden">
               <input className='antialiased outline-none h-full bg-white w-[310px] border border-r-0 border-[#B5B5B5] px-3 text-base  rounded-s-md' placeholder='Search by Title, Author, Keyword or ISBN' type="text" />
               <button style={styleTheme} className={`w-11 h-11 bg-[${theme.primary}] hover:bg-[#1F4A50] transition-all duration-300 flex items-center border border-[#B5B5B5] text-center justify-center text-white rounded-e-md`} type='button'><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
           </div>
           
-          <div className="float-right flex items-center gap-6">
-              <ul className='flex items-center gap-6 '>
+          <div className="float-right flex items-center gap-6 text-nowrap">
+              <ul className='flex items-center gap-6 max-md:hidden'>
                   <div className="flex flex-row items-center justify-center relative"><li className={`hover:text-black/55 transition-colors duration-500`}><a href="">Shop</a></li></div>
                   <div className="flex flex-row items-center justify-center relative"><li className='hover:text-black/55 transition-colors duration-500'><a href="">Categories</a></li></div>
                   <div className="flex flex-row items-center justify-center relative"><li className='hover:text-black/55 transition-colors duration-500'><a href="">My Favorities</a></li><span className='absolute -top-2 -right-4 bg-red-600 text-white text-xs w-5 h-5 flex justify-center items-center rounded-full cursor-default'>2</span></div>
                   <div className="flex flex-row items-center justify-center relative"><li className='hover:text-black/55 transition-colors duration-500 flex items-center justify-center'><a className='flex items-center justify-center z-20' href=""><FontAwesomeIcon icon={faCartShopping} /></a></li><span className='absolute -top-3 -right-4 bg-red-600 text-white text-xs w-5 h-5 flex justify-center items-center rounded-full cursor-default'>5</span></div>
               </ul>
-              <button className='px-6 text-[#CC0E0E] hover:text-[#CC0E0E] border-2 border-[#CC0E0E] hover:border-[#832c2c] rounded-sm py-1 transition-colors duration-500' type="button">Satış Yap</button> 
+              <button className='max-md:hidden text-nowrap px-6 text-[#CC0E0E] hover:text-[#CC0E0E] border-2 border-[#CC0E0E] hover:border-[#832c2c] rounded-sm py-1 transition-colors duration-500' type="button">Satış Yap</button> 
               {
                 isLoggedIn ? 
                 <div className="relative gap-5 flex flex-col">
@@ -39,7 +39,7 @@ function index() {
                       <FontAwesomeIcon className={ clickedUserModal ? 'absolute right-2 -rotate-180 transition-all duration-500' : 'absolute right-2 transition-all duration-500'} icon={faAngleDown} />
                   </button>
                   
-                  <Popover className={`${clickedUserModal ? 'visible opacity-100' : 'invisible opacity-0'} drop-shadow-md flex flex-col items-center transition-all duration-300 ease-out absolute w-[219px] h-[261px] bg-[#FFFFFF] top-14 right-0 border border-[#CCCCCC] text-black`}>
+                  <Popover className={`${clickedUserModal ? 'visible opacity-100' : 'invisible opacity-0'} z-10  drop-shadow-md flex flex-col items-center transition-all duration-300 ease-out absolute w-[219px] h-[261px] bg-[#FFFFFF] top-14 right-0 border border-[#CCCCCC] text-black`}>
                     <PopoverButton className="w-full h-[35px] bg-white border-b border-[#CCCCCC] rounded-t-[4px] hover:text-[#616161] text-start px-2">Kullanıcı Ayarları</PopoverButton>
                     <PopoverButton onClick={() => setClickedColorModal(!clickedColorModal)} className= {`${clickedColorModal ? 'text-[#285f66]': 'text-black'} mt-1 w-full h-[35px] bg-white border-b border-[#CCCCCC] hover:text-[#616161] text-start px-2`}>Görüntü Ayarları</PopoverButton>
                     <PopoverButton className="mt-1 w-full h-[35px] bg-white border-b border-[#CCCCCC] hover:text-[#616161] text-start px-2">Sipariş Geçmişi</PopoverButton>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ function index() {
 
   return (
     <div className="h-screen overflow-hidden bg-hero-pattern bg-cover bg-center flex items-center">
-      <div className="px-4 md:px-7 h-auto  max-sm:text-center max-lg:max-w-2xl max-sm:mx-auto max-sm:max-w-md  max-w-3xl xs:max-w-lg py-10 bg-[#F0EDEE]/90 flex flex-col gap-4 ml-4 md:ml-24 rounded-xl drop-shadow-md font-lalezar relative">
+      <div className="px-4 md:px-7 h-auto  max-sm:text-center max-sm:max-w-md max-sm:mx-auto sm:max-w-lg sm:mx-auto max-md:max-w-2xl md:max-w-2xl md:mx-auto max-lg:max-w-3xl max-lg:mx-auto lg:max-w-3xl  py-10 bg-[#F0EDEE]/90 flex flex-col gap-4 ml-4 md:ml-24 rounded-xl drop-shadow-md font-lalezar relative">
         <h1 style={stylePrimaryColor} className="text-2xl md:text-[40px]">
           Aradığınız Kitaplar Uygun Fiyatlarla Burada!
         </h1>
@@ -19,13 +19,17 @@ function index() {
         <h4 className="text-sm md:text-base text-[#0A090C]">
           Okudukça daha fazla kazan! Kitap alışverişini keyifle yapmak için seni bekliyoruz. Kitap sat, kitap al, bilgiyi paylaş.
         </h4>
-        <button
-          style={{ backgroundColor: theme.primary }}
-          className="w-[135px] text-white px-4 py-2 rounded-md hover:text-white/95 transition-all duration-200"
-          type="button"
-        >
-          Şimdi Keşfet
-        </button>
+        
+        <div className="flex w-full max-sm:justify-center max-md:justify-end md:justify-end lg:justify-end">
+          <button
+            style={{ backgroundColor: theme.primary }}
+            className="w-[135px] text-white px-4 py-2 rounded-md float-end hover:text-white/95 transition-all duration-200"
+            type="button"
+          >
+            Şimdi Keşfet
+          </button>
+        </div>
+        
       </div>
     </div>
 
